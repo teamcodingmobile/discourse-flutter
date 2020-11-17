ef7e0a4e8ceac2b70d313c1563c988f213b7b62d
David Rabassa <gestionarlaweb@gmail.com>
2020-11-17 10:38:09 +0100

    base pantalla

--- lib/src/pages/tab3_page.dart
+++ lib/src/pages/tab3_page.dart
@@ -1,27 +1,321 @@
-import 'package:discourse/src/services/users_service.dart';
-import 'package:discourse/src/widgets/bar_desing.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class Tab3Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // PROVIDER
-    Provider.of<UsersService>(context);
-    //
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text(
-          'Profile',
-          style: TextStyle(color: Colors.black),
-          textAlign: TextAlign.left,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(100),
+        child: AppBar(
+          flexibleSpace: _ColorAppBar(),
+          title: Column(
+            children: [
+              Container(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                  child: ListTile(
+                    title:
+                        Text('Nombre Usuario', style: TextStyle(fontSize: 20)),
+                    subtitle: Text('email'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      body: Stack(
-        children: [
-          BarDesing(),
+      body: Column(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    _BottomBarSimple(),
+                    ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage:
+                            AssetImage('assets/img/no-image.png'), // [index]
+                        radius: 28,
+                      ),
+                      title: Text('lo que sea'),
+                      subtitle: Text(' lo otro'),
+                    ),
+                    _BottomBar(),
+                    ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage:
+                            AssetImage('assets/img/no-image.png'), // [index]
+                        radius: 28,
+                      ),
+                      title: Text('lo que sea'),
+                      subtitle: Text(' lo otro'),
+                    ),
+                    _BottomBar(),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class _ColorAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: <Color>[
+            Color.fromRGBO(255, 192, 203, 1),
+            Color.fromRGBO(242, 232, 143, 1),
+          ])),
+    );
+  }
+}
+
+class _BottomBarSimple extends StatelessWidget {
+  //const _BottomBar({@required this.topic});
+
+  //final Topic topic;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 10.0),
+        child: Container(
+          decoration: BoxDecoration(color: const Color(0xfff8f8f8)),
+          padding: EdgeInsets.symmetric(vertical: 8.0),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: Column(
+              children: [
+                Container(
+                  child: CircleAvatar(
+                    backgroundImage:
+                        AssetImage('assets/img/no-image.png'), // [index]
+                    radius: 60,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 45.0),
+                          child: Row(
+                            children: <Widget>[
+                              Container(
+                                child: Text('Topics'
+                                    // '${topic.postsCount}',
+                                    ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 15.0),
+                          child: Row(
+                            children: <Widget>[
+                              Container(
+                                child: Text('Posts'
+                                    // '${topic.postsCount}',
+                                    ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 45.0),
+                          child: Row(
+                            children: <Widget>[
+                              Container(
+                                child: Text('Likes'
+                                    //'${topic.replyCount}'
+                                    ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                _TextFixed(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _TextFixed extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 45.0),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    child: Text('TOPICS'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15.0),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    child: Text('POSTS'
+                        // '${topic.postsCount}',
+                        ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+        Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 45.0),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    child: Text('LIKES'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _BottomBar extends StatelessWidget {
+  //const _BottomBar({@required this.topic});
+
+  //final Topic topic;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 10.0),
+        child: Container(
+          decoration: BoxDecoration(color: const Color(0xfff8f8f8)),
+          padding: EdgeInsets.symmetric(vertical: 8.0),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 45.0),
+                          child: Row(
+                            children: <Widget>[
+                              Container(
+                                child: Text('50'
+                                    //'${topic.postsCount}',
+
+                                    ),
+                              ),
+                              Container(
+                                child: Icon(
+                                  Icons.format_list_numbered,
+                                  size: 20.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 15.0),
+                          child: Row(
+                            children: <Widget>[
+                              Container(
+                                child: Text('4'
+                                    //'${topic.postsCount}',
+
+                                    ),
+                              ),
+                              Container(
+                                child: Icon(
+                                  Icons.view_list,
+                                  size: 20.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 45.0),
+                          child: Row(
+                            children: <Widget>[
+                              Container(
+                                child: Text(
+                                  ('23'),
+                                  //'${topic.replyCount}',
+                                ),
+                              ),
+                              Container(
+                                child: Icon(
+                                  Icons.view_list,
+                                  size: 20.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
